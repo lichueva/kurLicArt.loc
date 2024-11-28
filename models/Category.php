a74e2af7c4f2c4a2bd99c7a17dadaba2dd7a7876
lichueva <lyudka.lichueva@gmail.com>
2024-11-28 17:24:19 +0200

    category

--- models/Category.php
+++ models/Category.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace app\models;
+use yii\data\Pagination;
 
 use Yii;
 
@@ -39,5 +40,42 @@ class Category extends \yii\db\ActiveRecord
             'id' => 'ID',
             'title' => 'Title',
         ];
+    }
+
+    public function getArticles()
+    {
+        return $this->hasMany(Article::className(), ['category_id' => 'id']);
+    }
+
+    public function getArticlesCount()
+    {
+        return $this->getArticles()->count();
+    }
+
+    public static function getAll()
+    {
+        return Category::find()->all();
+    }
+
+    public static function getArticlesByCategory($id)
+    {
+        // build a DB query to get all articles
+        $query = Article::find()->where(['category_id' => $id]);
+
+        // get the total number of articles (but do not fetch the article data yet)
+        $count = $query->count();
+
+        // create a pagination object with the total count
+        $pagination = new Pagination(['totalCount' => $count, 'pageSize' => 6]);
+
+        // limit the query using the pagination and retrieve the articles
+        $articles = $query->offset($pagination->offset)
+            ->limit($pagination->limit)
+            ->all();
+
+        $data['articles'] = $articles;
+        $data['pagination'] = $pagination;
+
+        return $data;
     }
 }
--- modules/admin/controllers/ArticleController.php
+++ modules/admin/controllers/ArticleController.php
@@ -165,7 +165,7 @@ class ArticleController extends Controller
     public function actionSetCategory($id)
     {
         $article = $this->findModel($id);
-        $selectedCategory = $article->category->id;
+        $selectedCategory = ($article->category) ? $article->category->id : '0';
         $categories = ArrayHelper::map(Category::find()->all(), 'id', 'title');
 
         if(Yii::$app->request->isPost)
