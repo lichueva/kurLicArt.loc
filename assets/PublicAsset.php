da714ff599f5fc349ee9ba2f97beb39d54067ee8
lichueva <lyudka.lichueva@gmail.com>
2024-11-28 23:21:05 +0200

    delete mark

--- assets/PublicAsset.php
+++ assets/PublicAsset.php
@@ -19,14 +19,14 @@ class PublicAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        "public/css/bootstrap.min.css",
+        
         "public/css/font-awesome.min.css",
         "public/css/animate.min.css",
         "public/css/owl.carousel.css",
         "public/css/owl.theme.css",
         "public/css/owl.transitions.css",
         "public/css/style.css",
-        "public/css/responsive.css",
+        "public/css/responsive.css", "public/css/bootstrap.min.css",
     ];
     public $js = [
         //        "public/js/jquery-1.11.3.min.js",
